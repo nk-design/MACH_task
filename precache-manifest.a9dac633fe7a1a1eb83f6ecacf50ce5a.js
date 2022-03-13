8b7fc66bd0f09a04c81d383e4a1caebd23d680e2
Mykola Kovtun <mykola.kovtun@royalcanin.com>
2022-03-14 00:25:01 +0200

    Updates

---
+++ precache-manifest.a9dac633fe7a1a1eb83f6ecacf50ce5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aa172193a675252bdccf28bdd76d44f",
+    "url": "/MACH_task/index.html"
+  },
+  {
+    "revision": "27f6ddd86449dc684e3b",
+    "url": "/MACH_task/static/css/main.93fb9dd5.chunk.css"
+  },
+  {
+    "revision": "f25a2b0b422244274f94",
+    "url": "/MACH_task/static/js/2.93fa954c.chunk.js"
+  },
+  {
+    "revision": "27f6ddd86449dc684e3b",
+    "url": "/MACH_task/static/js/main.81a8c34c.chunk.js"
+  },
+  {
+    "revision": "cf7864bd8e4ad55e0507",
+    "url": "/MACH_task/static/js/runtime~main.2b6c8a11.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/MACH_task/static/media/crown.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/MACH_task/static/media/shopping-bag.30f07071.svg"
+  }
+]);
